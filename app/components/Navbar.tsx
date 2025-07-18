26e8a34a37b61ca312191f6006d182a76b24305b
Ferjo <ferjobyte@gmail.com>
2025-07-18 01:58:45 -0300

    animating icons navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,11 +13,14 @@ export default function Navbar() {
     return (
         <nav className='relative flex justify-between items-center m-4'>
             <div>
-                <a href="" className="logo">FERJO</a>
+                <a href="" className="logo">
+                    <span className='block sm:hidden'>FERJO</span>
+                    <span className='hidden sm:block'>FERNANDEZ JONATHAN</span>
+                </a>
             </div>
             <ul className='hidden sm:flex'>
-                <li><a href="#About me">About me</a></li>
-                <li><a href="#Projects">Projects</a></li>
+                <li className='pr-8'><a href="#About me">About me</a></li>
+                <li className='pr-8'><a href="#Projects">Projects</a></li>
                 <li><a href="#Contact">Contact</a></li>
             </ul>
 
@@ -27,17 +30,20 @@ export default function Navbar() {
                     src={isMenuOpen ?
                      '/icon-menu-back.svg' :
                       '/icon-menu.svg'} 
-                      alt="" />
+                      alt=""
+                      className='transform transition-transform duration-75 ease-in-out'
+                      style={{ transform: isMenuOpen ? "rotate(360deg)" : "rotate(0deg)" }}
+                      />
                 </button>
             </div>
             
 
             {isMenuOpen && (
-               <div className='absolute top-16 right-4'>
-               <ul>
-                    <li><a href="#About me">About me</a></li>
-                    <li><a href="#Projects">Projects</a></li>
-                    <li><a href="#Contact">Contact</a></li>
+               <div className='absolute top-16 right-4 mr-2  transition-all duration-700 ease-in-out'>
+               <ul className='animate-fade-in'>
+                    <li className='p-2'><a href="#About me">About me</a></li>
+                    <li className='p-2'><a href="#Projects">Projects</a></li>
+                    <li className='p-2'><a href="#Contact">Contact</a></li>
                 </ul>
                 </div>
              
