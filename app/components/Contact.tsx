56407d52953302456409f9aaa7ad97dca925809f
Ferjo <ferjobyte@gmail.com>
2025-08-22 23:51:02 -0300

    Contact structure done

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,25 +1,26 @@
 export default function Contact() {
     return (
-        <section className="md:flex items-center">
-            <div className="font-k2d basis-1/2 grid justify-items-center">
+        <section className="md:flex items-center ">
+            <div className="font-k2d basis-1/2 grid justify-items-center py-20">
                 <p className="text-xl">WANT TO COLLABORATE?</p>
                 <h3 className="text-5xl font-medium ">LET'S <br />
                     <span className="text-[#91F900]">CONNECT</span>
                 </h3>
             </div>
-            <div className="bg-white md:basis-1/2 grid justify-items-center">
-                <h4 className="font-k2d font-semibold text-lg text-black ">CONTACT
+            <div className="bg-white md:basis-1/2 px-12 lg:px-32">
+                <h4 className="font-k2d font-semibold text-lg text-black py-8 text-center">CONTACT
                     <span className="text-[#91F900] bg-black"> FORM </span>
                 </h4>
+
                 <form action="/submit"
-                    className="font-inter md:1/3">
+                    className="font-inter">
                     <label htmlFor="FullName"
                         className="font-medium text-black"
                     >Name</label><br />
                     <input type="text"
                         id="FullName" name="FullName"
                         placeholder="Your name here" required
-                        className="lg:w-full border-b-2 border-[#C0C0C0]
+                        className="w-full border-b-2 border-[#C0C0C0]
                         placeholder:text-sm placeholder:font-light" /><br />
 
                     <label htmlFor="email"
@@ -28,7 +29,7 @@ export default function Contact() {
                     <input type="mail"
                         id="mail" name="mail"
                         placeholder="Your mail here" required
-                        className="lg:w-full border-b-2 border-[#C0C0C0]
+                        className="w-full  border-b-2 border-[#C0C0C0]
                         placeholder:text-sm placeholder:font-light" /><br />
 
                     <label htmlFor="message"
@@ -37,12 +38,17 @@ export default function Contact() {
                     <textarea id="message" name="message" rows={3}
                         placeholder="Leave us your message"
                         required
-                        className="lg:w-full border-b-2 border-[#C0C0C0]
-                        placeholder:text-sm placeholder:font-light"
-                    /><textarea /> <br />
-
-                    <button type="submit">Submit</button>
+                        className="w-full border-b-2 border-[#C0C0C0]
+                        placeholder:text-sm placeholder:font-light resize-none"
+                    /> <br />
+                    <div className="flex justify-center py-8">
+                        <button type="submit"
+                            className=""
+                        >Submit</button>
+                    </div>
                 </form>
+
+
             </div>
         </section>
     )
